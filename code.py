47e151ac18a05ff9605fd2fb7deea0ce5889ad66
DenisRudakov0 <79634508+DenisRudakov0@users.noreply.github.com>
2021-12-31 02:57:43 +0300

    add check nan

--- code.py
+++ code.py
@@ -1,26 +1,25 @@
 import pandas as pd
-
 import requests
 from bs4 import BeautifulSoup
 
 data_excel = pd.read_excel('./image.xlsx')
 lisenin = data_excel.to_dict(orient='records')
 for i in lisenin:
-    url = i['Url']
-    name = i['Артикул']
-    if url != 'nan':
+    try:
+        url = i['Url']
+        name = i['Артикул']
         response = requests.get(url)
         soup = BeautifulSoup(response.text, 'lxml')
         quotes = soup.find('div', class_='product-item-detail-slider-image')
         print('---------------------------------------------------')
         string = str(quotes)
         img = 'https://vorota.moscow' + string[string.index('src="') + 5:string.index('.jpg') + 4]
-        
         # image download
         print(name, url)
         p = requests.get(img)
         out = open(str(name) + '.jpg', "wb")
         out.write(p.content)
         out.close()
-
+    except:
+        print('nan')
     
