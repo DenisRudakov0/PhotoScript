f6683aa47fff453c8b9b3acae889270585a17fc6
DenisRudakov0 <79634508+DenisRudakov0@users.noreply.github.com>
2021-12-31 02:25:46 +0300

    download image

--- code.py
+++ code.py
@@ -7,13 +7,20 @@ data_excel = pd.read_excel('./image.xlsx')
 lisenin = data_excel.to_dict(orient='records')
 for i in lisenin:
     url = i['Url']
+    name = i['Артикул']
     if url != 'nan':
         response = requests.get(url)
         soup = BeautifulSoup(response.text, 'lxml')
         quotes = soup.find('div', class_='product-item-detail-slider-image')
         print('---------------------------------------------------')
         string = str(quotes)
-        print('https://vorota.moscow' + string[string.index('src="') + 5:string.index('.jpg') + 4])
-
+        img = 'https://vorota.moscow' + string[string.index('src="') + 5:string.index('.jpg') + 4]
+        
+        # image download
+        print(name, url)
+        p = requests.get(img)
+        out = open(str(name) + '.jpg', "wb")
+        out.write(p.content)
+        out.close()
 
     
