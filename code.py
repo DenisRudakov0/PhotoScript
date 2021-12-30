f8bf7a3d71590ac51ee2dae1f2abf174271b7529
DenisRudakov0 <79634508+DenisRudakov0@users.noreply.github.com>
2021-12-31 02:19:09 +0300

    add parser site url image

--- code.py
+++ code.py
@@ -1,6 +1,19 @@
 import pandas as pd
 
+import requests
+from bs4 import BeautifulSoup
+
 data_excel = pd.read_excel('./image.xlsx')
-lisenin = data_excel.to_dict(orient='record')
+lisenin = data_excel.to_dict(orient='records')
 for i in lisenin:
-    print(i['Артикул'], i['Наименование'])
+    url = i['Url']
+    if url != 'nan':
+        response = requests.get(url)
+        soup = BeautifulSoup(response.text, 'lxml')
+        quotes = soup.find('div', class_='product-item-detail-slider-image')
+        print('---------------------------------------------------')
+        string = str(quotes)
+        print('https://vorota.moscow' + string[string.index('src="') + 5:string.index('.jpg') + 4])
+
+
+    
