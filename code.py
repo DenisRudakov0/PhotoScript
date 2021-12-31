093c06f3fb43147ca5c20dde83e404fc343b06f3
DenisRudakov0 <79634508+DenisRudakov0@users.noreply.github.com>
2021-12-31 03:31:51 +0300

    modification code add function

--- code.py
+++ code.py
@@ -3,26 +3,29 @@ import requests
 from bs4 import BeautifulSoup
 from PIL import Image
 
+def download_image(data_excel, url_save):
+    lisenin = data_excel.to_dict(orient='records')
+    for i in lisenin:
+        try:
+            url = i['Url']
+            name = i['Артикул']
+            response = requests.get(url)
+            soup = BeautifulSoup(response.text, 'lxml')
+            quotes = soup.find('div', class_='product-item-detail-slider-image')
+            print('---------------------------------------------------')
+            string = str(quotes)
+            file = 'https://vorota.moscow' + string[string.index('src="') + 5:string.index('.jpg') + 4]
+            
+            # image download
+            print(name, url)
+            p = requests.get(file)
+            out = open(url_save + str(name) + '.jpg', "wb")
+            out.write(p.content)
+            out.close()
+        except:
+            print('nan')
+
 data_excel = pd.read_excel(input("Укажите путь к excel файлу: "))
 url_save = input('Укажите места для сохранения изображений: ')
-lisenin = data_excel.to_dict(orient='records')
-for i in lisenin:
-    try:
-        url = i['Url']
-        name = i['Артикул']
-        response = requests.get(url)
-        soup = BeautifulSoup(response.text, 'lxml')
-        quotes = soup.find('div', class_='product-item-detail-slider-image')
-        print('---------------------------------------------------')
-        string = str(quotes)
-        file = 'https://vorota.moscow' + string[string.index('src="') + 5:string.index('.jpg') + 4]
-        
-        # image download
-        print(name, url)
-        p = requests.get(file)
-        out = open(url_save + str(name) + '.jpg', "wb")
-        out.write(p.content)
-        out.close()
-    except:
-        print('nan')
-    
+download_image(data_excel, url_save)
+
